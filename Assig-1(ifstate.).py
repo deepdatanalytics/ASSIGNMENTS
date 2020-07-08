5ac8b98b2673455dfca67d10ee4489f9e11f9b02
bruce <derinfokus@gmail.com>
2020-07-08 17:25:47 +0300

    Assig if state has been done.

---
+++ Assig-1(ifstate.).py
@@ -0,0 +1,9 @@
+name = input("Please write your name: ")
+
+my_name = "bruce"
+
+if name == "bruce":
+    print(f"Hello {my_name}! The password is : asdf")
+
+else :
+    print(f"Hello {name}! See you later.")
